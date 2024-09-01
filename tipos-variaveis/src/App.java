0fb349209e204649191f86471f9e2b81b838ad93
danielmcampos <danielmcamp14@gmail.com>
2024-09-01 20:35:18 -0300

    atribuição

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String meuNome = "Daniel Martins";
+        
+        int numeroNota = 10;
+
+        numeroNota = 21;
+
+        System.out.print(numeroNota);
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+
     }
 }
